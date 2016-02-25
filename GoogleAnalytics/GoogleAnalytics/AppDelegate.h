72efd18887e601e9c01a7ade326743974e699e76
ManhNQ <manhnq@asiantech.vn>
2016-02-25 16:25:19 +0700

    init

---
+++ GoogleAnalytics/GoogleAnalytics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleAnalytics
+//
+//  Created by Asiantech on 2/25/16.
+//  Copyright (c) 2016 Asiantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleAnalytics/GoogleAnalytics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleAnalytics
+//
+//  Created by Asiantech on 2/25/16.
+//  Copyright (c) 2016 Asiantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
